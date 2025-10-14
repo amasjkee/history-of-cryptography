67a6ce0328f1b0e571903e6ca8aa7c1c7171b37f
274 <asamkioto@yandex.ru>
2025-10-14 20:05:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/history-of-cryptography/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
